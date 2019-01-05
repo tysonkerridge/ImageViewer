e206acb540a9a2bb43516532e1dea248fcc53c86
Tyson Kerridge <tysonbehere@hotmail.com>
2019-01-05 15:52:00 +1000

    Support different iPhone X (those with notch) sizes... Ideally this would be done using safe areas instead...

--- ImageViewer/Source/Extensions/UIScreen.swift
+++ ImageViewer/Source/Extensions/UIScreen.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 public extension UIScreen {
+    
+    private static var iPhoneXRSize: CGSize { return CGSize(width: 828, height: 1792) }
+    private static var iPhoneXSSize: CGSize { return CGSize(width: 1125, height: 2436) }
+    private static var iPhoneXSMaxSize: CGSize { return CGSize(width: 1242, height: 2688) }
+    
     public class var hasNotch: Bool {
         // This will of course fail if Apple produces an notchless iPhone with these dimensions,
         // but is the simplest detection mechanism so far.
-        return main.nativeBounds.size == CGSize(width: 1125, height: 2436)
+        switch main.nativeBounds.size {
+        case iPhoneXSSize,
+             iPhoneXRSize,
+             iPhoneXSMaxSize:
+            return true
+        default:
+            return false
+        }
     }
 }
